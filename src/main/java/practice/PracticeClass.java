030407536e03287c857e1bdcdb8a7ee4bfb29a56
sayedsa <sadatsayedibrahim@gmail.com>
2022-11-23 19:34:57 -0800

    updated

---
+++ src/main/java/practice/PracticeClass.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * int i = 0; while (i<10) { System.out.println("from 0 ---9----" +i); i++; }
+		 * 
+		 */
+		int b = 1;
+		do {
+			System.out.println(b);;
+		 b++; }
+			while (b<10);{	
+				
+			}
+
+	}
+
+}
